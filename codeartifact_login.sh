acd426425a1da84da9441e788e8be7b091140d6f
Dario Ferrer <dario.ferrer@pollen.co>
2021-10-22 12:23:24 +0200

    Create codeartifact_login.sh
    This will login into codeartifact for npm and then scope it only to selected scopes so that the rest comes from https://registry.npmjs.org

---
+++ codeartifact_login.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rm -f ~/.npmrc
+aws codeartifact login --tool npm --domain $DOMAIN --domain-owner $AWS_ACCOUNT --repository $ARTIFACT_REPO
+npm config set registry https://registry.npmjs.org/
+ARTIFACT_URL=https://${DOMAIN}-${AWS_ACCOUNT}.d.codeartifact.${AWS_DEFAULT_REGION}.amazonaws.com/npm/${ARTIFACT_REPO}/
+for scope in ${NPM_SCOPES} ; do
+  npm config set @${scope}:registry ${ARTIFACT_URL}
+done
