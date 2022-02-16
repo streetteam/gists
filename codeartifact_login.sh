e473f4b678b27a7021c1a8e13ae30640934f7d6b
Jaycee <jaycee.cheong@pollen.co>
2022-02-16 08:51:51 +0000

    UNI-2292 Use get-repository-endpoint operation from aws codeartifact command to construct artifact url

--- codeartifact_login.sh
+++ codeartifact_login.sh
@@ -5,7 +5,7 @@ set -eux
 rm -f ~/.npmrc
 aws codeartifact login --tool npm --domain $DOMAIN --domain-owner $AWS_ACCOUNT --repository $ARTIFACT_REPO
 npm config set registry https://registry.npmjs.org/
-ARTIFACT_URL=https://${DOMAIN}-${AWS_ACCOUNT}.d.codeartifact.${AWS_DEFAULT_REGION}.amazonaws.com/npm/${ARTIFACT_REPO}/
+ARTIFACT_URL=$(aws codeartifact get-repository-endpoint --domain ${DOMAIN} --domain-owner ${AWS_ACCOUNT} --repository ${ARTIFACT_REPO} --format npm)
 for scope in ${NPM_SCOPES} ; do
   npm config set @${scope}:registry ${ARTIFACT_URL}
 done
