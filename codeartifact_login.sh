b8021e0c718bfcd85fe141e9aa593fbc5d732cfc
Dario Ferrer <dario.ferrer@pollen.co>
2022-06-28 13:21:29 +0200

    Update codeartifact_login.sh

--- codeartifact_login.sh
+++ codeartifact_login.sh
@@ -9,8 +9,11 @@ TOKEN=$(aws codeartifact get-authorization-token --domain $DOMAIN --domain-owner
 ARTIFACT_URL=$(aws codeartifact get-repository-endpoint --domain ${DOMAIN} --domain-owner ${AWS_ACCOUNT} --repository ${ARTIFACT_REPO} --format npm --output text)
 ##Strip https:// from the URL to set the token auth url in the config
 DOMAIN_ONLY_URL=$(echo ${ARTIFACT_URL}|sed 's#https://##')
-##Set the specific domains we will pull from Codeartifact
-echo "//${DOMAIN_ONLY_URL}:_authToken=${TOKEN}" > ~/.npmrc
+##Extra params from common_lib.packaging.credential.NpmLoginOverride.get_npmrc_content
+echo "//${DOMAIN_ONLY_URL}:always-auth=true" > ~/.npmrc
+echo "//${DOMAIN_ONLY_URL}:_authToken=${TOKEN}" >> ~/.npmrc
+echo "legacy-peer-deps=true" >> ~/.npmrc
+
 for scope in ${NPM_SCOPES} ; do
   npm config set @${scope}:registry ${ARTIFACT_URL}
 done
